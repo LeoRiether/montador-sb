a69b397a92ca9623c2ed8ed42739e6d0825b046d
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2023-01-02 03:40:57 -0300

    Generate object code

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -55,8 +55,40 @@ SymbolTable build_symbol_table(const vector<Line> &lines) {
 // Second pass of the codegen
 vector<uint16_t> generate_machine_code(const vector<Line> &lines,
                                        const SymbolTable &symbols) {
-    UNUSED(lines);
-    UNUSED(symbols);
-    // TODO:
-    return {};
+    vector<uint16_t> machine_code;
+    unsigned int line_counter = 1;
+    for (const auto& line : lines) {
+        // Instruction
+        if (line.which == Line::IsInstruction) {
+            Token instruction = line.data[0];
+            InstructionData instr_data = instructions.at(instruction);
+            machine_code.push_back(instr_data.opcode);
+            for (size_t i = 1; i < instr_data.size; i++) {
+                Token argument = line.data[i];
+                if (!symbols.count(argument)) {
+                    throw AssemblerError(
+                        "Sêmantico",
+                        "O rótulo <" + argument + "> não existe.",
+                        line_counter, i + 1);
+                }
+                uint16_t memory_address = symbols.find(argument)->second;
+                machine_code.push_back(memory_address);
+            }
+        }
+        // Directive
+        else if (line.which == Line::IsDirective) {
+            Token directive = line.data[0];
+            if (directive == "SPACE") {
+                for (int i = 0; i < line.num; i++) {
+                    machine_code.push_back(0);
+                }
+            } else if (directive == "CONST") {
+                machine_code.push_back(line.num);
+            }
+        }
+
+        line_counter ++;
+    }
+
+    return machine_code;
 }
