7b224626b4a6231692ef3d0a4af5b6f9a7c5bfcd
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2023-01-02 13:40:34 -0300

    More macro tests

--- tests/preprocessor.cpp
+++ tests/preprocessor.cpp
@@ -55,7 +55,7 @@ TEST_CASE("Stage 1 Preprocessor: IFs", "[preprocessor][ifs]") {
 }
 
 TEST_CASE("Stage 2 Preprocessor: MACROs", "[preprocessor][macros]") {
-    SKIP_SECTION("Basic macro functionality") {
+    SECTION("Basic macro functionality") {
         vector<Token> input = {
             t("M"), t(":"), t("MACRO"), t("&X"), t("\n"),
             t("LOAD"), t("&X"), t("\n"),
@@ -73,6 +73,79 @@ TEST_CASE("Stage 2 Preprocessor: MACROs", "[preprocessor][macros]") {
             t("A"), t("B"), t("C"), t("\n"),
         };
 
-        REQUIRE(preprocess_equs_ifs(input) == expected);
+        REQUIRE(preprocess_macros(input) == expected);
+    }
+    SECTION("Simple macro without arguments") {
+        vector<Token> input = {
+            t("FIM"), t(":"), t("MACRO"), t("\n"),
+            t("JMP"), t("END"), t("\n"),
+            t("ENDMACRO"), t("\n"),
+            t("X"), t("Y"), t("\n"),
+            t("FIM"), t("\n"),
+            t("W"), t("\n"),
+            t("FIM"), t("\n"),
+            t("Z"), t("\n"),
+        };
+        vector<Token> expected = {
+            t("X"), t("Y"), t("\n"),
+            t("JMP"), t("END"), t("\n"),
+            t("W"), t("\n"),
+            t("JMP"), t("END"), t("\n"),
+            t("Z"), t("\n"),
+        };
+
+        REQUIRE(preprocess_macros(input) == expected);
+    }
+    SECTION("Macro with 3 arguments, with argument names equal to local variables names") {
+        vector<Token> input = {
+            t("SUM"), t(":"), t("MACRO"), t("&X"), t("&Y"), t("&Z"), t("\n"),
+            t("ADD"), t("&X"), t("\n"),
+            t("ADD"), t("&Y"), t("\n"),
+            t("ADD"), t("&Z"), t("\n"),
+            t("ENDMACRO"), t("\n"),
+            t("ADD"), t("A"), t("\n"),
+            t("SUM"), t("Y"), t("Z"), t("X"), t("\n"),
+            t("ADD"), t("B"), t("\n"),
+            t("SUM"), t("A"), t("B"), t("C"), t("\n"),
+            t("ADD"), t("C"), t("\n"),
+        };
+        vector<Token> expected = {
+            t("ADD"), t("A"), t("\n"),
+            t("ADD"), t("Y"), t("\n"),
+            t("ADD"), t("Z"), t("\n"),
+            t("ADD"), t("X"), t("\n"),
+            t("ADD"), t("B"), t("\n"),
+            t("ADD"), t("A"), t("\n"),
+            t("ADD"), t("B"), t("\n"),
+            t("ADD"), t("C"), t("\n"),
+            t("ADD"), t("C"), t("\n"),
+        };
+
+        REQUIRE(preprocess_macros(input) == expected);
+    }
+    SECTION("Macro with more than 3 arguments") {
+        vector<Token> input = {
+            t("SUM"), t(":"), t("MACRO"), t("&X"), t("&Y"), t("&W"), t("&Z"), t("\n"),
+            t("ADD"), t("&X"), t("\n"),
+            t("ADD"), t("&Y"), t("\n"),
+            t("ADD"), t("&W"), t("\n"),
+            t("ADD"), t("&Z"), t("\n"),
+            t("ENDMACRO"), t("\n"),
+        };
+
+        REQUIRE_THROWS(preprocess_macros(input));
+    }
+    SECTION("Calling macro with non matching arguments") {
+        vector<Token> input = {
+            t("SUM"), t(":"), t("MACRO"), t("&X"), t("&Y"), t("&Z"), t("\n"),
+            t("ADD"), t("&X"), t("\n"),
+            t("ADD"), t("&Y"), t("\n"),
+            t("ADD"), t("&W"), t("\n"),
+            t("ADD"), t("&Z"), t("\n"),
+            t("ENDMACRO"), t("\n"),
+            t("SUM"), t("A"), t("B"), t("\n"),
+        };
+
+        REQUIRE_THROWS(preprocess_macros(input));
     }
 }
